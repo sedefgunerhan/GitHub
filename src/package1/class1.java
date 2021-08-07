76822b5d661ccce5b5ba069deb83a59aa033d57a
sedefgunerhan <88284778+sedefgunerhan@users.noreply.github.com>
2021-08-07 14:48:44 -0400

    Update class1.java

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -8,7 +8,7 @@ public class class1 {
         System.out.println("Batch 35/36 learn GitHub");
 
         System.out.println("We like Git but we will like more when we have more practice ");
-
+         System.out.println("this is remote repo CODE"):   
     }
 
 
